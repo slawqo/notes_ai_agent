5b37ab942c479f1df63c671cc84f7fd8d9d4eef6
Slawek Kaplonski <skaplons@redhat.com>
2025-07-18 09:46:50 +0200

    [Tools] Exclude .trash folder(s) from processing by the bash script
    Signed-off-by: Slawek Kaplonski <skaplons@redhat.com>

--- tools/process_documents.sh
+++ tools/process_documents.sh
@@ -5,7 +5,7 @@
 
 DIR="${1:-.}"
 
-find "$DIR" -type f -name "*.md" | while read -r file; do
+find "$DIR" -type f -name "*.md" -not -path "*/.trash/*" | while read -r file; do
     echo "Processing: $file"
     notes_ai_agent "$file"
 done
